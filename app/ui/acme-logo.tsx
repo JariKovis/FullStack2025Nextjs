a2acb2510263a2c2fbb38d675adecf051cc31d5b
Jari Kovis <itse@jarikovalainen.com>
2025-01-19 16:52:44 +0200

    Chapter 9 completed

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,7 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { CircleStackIcon } from "@heroicons/react/24/solid";
+import { lusitana } from "@/app/ui/fonts";
+import LaureaLogo from "@/public/Laurealogo.svg";
 
 export default function AcmeLogo() {
   return (
@@ -7,7 +9,9 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <CircleStackIcon className="h-12 w-12" />
+      {/* <img src={LaureaLogo.src} className="h-12 w-12" /> */}
+      <p className="text-[44px]">Laurea</p>
     </div>
   );
 }
